e06363cd5e0510d3af154358e43bd7e83587e918
Sandy <sandysouzasantos@gmail.com>
2017-08-14 11:34:41 -0300

    Add places library, markers and infowindows

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
-}
+
+var myViewModel = function () {
+
+};
+
+ko.applyBindings(new myViewModel());
+
+
