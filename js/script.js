59c0e425ff601ee03293be4f18ade94a4dbc93d1
Sandy Souza Santos <sandy.capgemini@sebraemg.com.br>
2017-09-19 17:12:23 -0300

    Finish day

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var myViewModel = function () {
+var markers;
 
-    this.move = function () {
+var myViewModel = function () {
+    var self = this;
+
+    markers = ko.observableArray();
+
+
+
+
+    self.move = function () {
         $('.portfolio').toggleClass('portfolio-only');
     }
 };
