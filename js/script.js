f6cb8c34c43cf7debc351ede3360f3cb0dbb8f72
Sandy Souza Santos <sandy.capgemini@sebraemg.com.br>
2017-09-20 11:43:59 -0300

    Add click bind to li

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ var markers = ko.observableArray();
 
 var myViewModel = function () {
     var self = this;
-
-
-
+    self.lastInterest = ko.observable();
 
     self.move = function () {
         $('.portfolio').toggleClass('portfolio-move');
-    }
+    };
+
+    self.logMouseOver = function(place) {
+        self.lastInterest(place);
+    };
 };
 
 ko.applyBindings(new myViewModel());
