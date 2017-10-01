e18ec12435d463acadd575bf15fd22f4c1dc36a3
Sandy Santos <sandysouzasantos@gmail.com>
2017-10-01 09:22:18 -0300

    Adiciona o bounce no click da lista

--- js/script.js
+++ js/script.js
@@ -3,14 +3,9 @@ var markers = ko.observableArray();
 var myViewModel = function () {
     var self = this;
 
-
-
-
     self.move = function () {
         $('.portfolio').toggleClass('portfolio-move');
     }
 };
 
 ko.applyBindings(new myViewModel());
-
-
